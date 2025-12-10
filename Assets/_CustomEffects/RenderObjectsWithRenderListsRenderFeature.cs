ee2e66f1d7fc023bad80b6b1653e67e6e1f3dbbf
Sam Yu <sam.yu@sam.yu-70VM>
2025-12-10 11:56:16 +0000

    Tutorial working

--- Assets/_CustomEffects/RenderObjectsWithRenderListsRenderFeature.cs
+++ Assets/_CustomEffects/RenderObjectsWithRenderListsRenderFeature.cs
@@ -9,49 +9,64 @@ public class RenderObjectsWithRenderListsRenderFeature : ScriptableRendererFeatu
 {
     class RenderObjectsWithRenderListCustomPass : ScriptableRenderPass
     {
+
+        private LayerMask m_LayerMask;
+        private Material m_Material;
+        
         private List<ShaderTagId> shaderTagIDList = new List<ShaderTagId>();
+        
+        
         
         //Configure the properties from our pass here
         public void Setup(LayerMask layerMask, Material material)
         {
-            
+            m_LayerMask = layerMask;
+            m_Material = material;
         }
         
         //We'll store the data that our RenderGraph pass needs here 
         private class MyPassData
         {
+            public RendererListHandle m_RendererListHandle;
+            public Material m_Material;
         }
 
         //We'll configure our Render List here: set up the shaders, filtering&drawing settings, etc. 
         private void InitRendererList(ContextContainer frameData, ref MyPassData passData, RenderGraph renderGraph)
         {
-            // UniversalRenderingData renderingData = frameData.Get<UniversalRenderingData>();
-            // UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
-            // UniversalLightData lightData = frameData.Get<UniversalLightData>();
-            //
-            // var sortFlags = cameraData.defaultOpaqueSortFlags;
-            // RenderQueueRange renderQueueRange = RenderQueueRange.opaque;
-            //
-            // int ignoreMask = ~layerMask;
-            // FilteringSettings filteringSettings = new FilteringSettings(renderQueueRange, ignoreMask); //gathers all objects that can be rendered as normal
-            //
-            // ShaderTagId[] forwardOnlyShaderTagIds = new[] //LightMode ShaderLab pass
-            // {
-            //     new ShaderTagId("UniversalForwardOnly"),
-            //     new ShaderTagId("UniversalForward")
-            // };
-            //
-            // shaderTagIDList.Clear();
-            //
-            // foreach (var sid in forwardOnlyShaderTagIds)
-            // {
-            //     shaderTagIDList.Add(sid);
-            // }
+            UniversalRenderingData renderingData = frameData.Get<UniversalRenderingData>();
+            UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+            UniversalLightData lightData = frameData.Get<UniversalLightData>();
+            
+            var sortFlags = cameraData.defaultOpaqueSortFlags;
+            RenderQueueRange renderQueueRange = RenderQueueRange.opaque;
+            
+            int ignoreMask = ~m_LayerMask;
+            FilteringSettings filteringSettings = new FilteringSettings(renderQueueRange, ignoreMask); //gathers all objects that can be rendered as normal
+            
+            ShaderTagId[] forwardOnlyShaderTagIds = new[] //LightMode ShaderLab pass
+            {
+                new ShaderTagId("UniversalForwardOnly"),
+                new ShaderTagId("UniversalForward")
+            };
+            
+            shaderTagIDList.Clear();
+            
+            foreach (var sid in forwardOnlyShaderTagIds)
+            {
+                shaderTagIDList.Add(sid);
+            }
+
+            DrawingSettings drawingSettings = RenderingUtils.CreateDrawingSettings(shaderTagIDList, renderingData, cameraData, lightData, sortFlags);
+            var param = new RendererListParams(renderingData.cullResults, drawingSettings, filteringSettings);
+            passData.m_RendererListHandle = renderGraph.CreateRendererList(param);
         }
         
         //Actually execute our draw commands. Will call it directly from RecordRenderGraph.
         static void ExecutePass(MyPassData data, RasterGraphContext context)
         {
+            context.cmd.DrawProcedural(Matrix4x4.identity, data.m_Material, 0, MeshTopology.Triangles, 3, 1);
+            context.cmd.DrawRendererList(data.m_RendererListHandle);
         }
         
         //Here we tell RenderGraph our inputs our outputs & register our passes. We don't execute command buffers.
@@ -63,7 +78,11 @@ public class RenderObjectsWithRenderListsRenderFeature : ScriptableRendererFeatu
             // This adds a raster render pass to the graph, specifying the name and the data type that will be passed to the ExecutePass function.
             using (var builder = renderGraph.AddRasterRenderPass<MyPassData>(passName, out var passData))
             {
-                
+                InitRendererList(frameData, ref passData, renderGraph);
+                passData.m_Material = m_Material;
+
+                builder.UseRendererList(passData.m_RendererListHandle);
+                builder.SetRenderAttachment(resourceData.activeColorTexture, 0);
                 
                 // Set up our function that will actually run our command buffers. This is what RG runs when executing the pass. 
                 builder.SetRenderFunc((MyPassData data, RasterGraphContext context) => ExecutePass(data, context));
@@ -72,8 +91,11 @@ public class RenderObjectsWithRenderListsRenderFeature : ScriptableRendererFeatu
     }
 
     RenderObjectsWithRenderListCustomPass m_ScriptablePass;
-    
 
+    public LayerMask layerMask;
+    public Material material;
+    
+    
     /// <inheritdoc/>
     public override void Create()
     {
@@ -85,9 +107,11 @@ public class RenderObjectsWithRenderListsRenderFeature : ScriptableRendererFeatu
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        if (renderingData.cameraData.cameraType == CameraType.Game)
-        {
+        // Only add this to the game view 
+        // if (renderingData.cameraData.cameraType == CameraType.Game)
+        // {
+            m_ScriptablePass.Setup(layerMask, material);
             renderer.EnqueuePass(m_ScriptablePass);
-        }
+        // }
     }
 }
